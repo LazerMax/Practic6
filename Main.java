93ede3f75d2b0e2e92e70996fd5d532e8c075e6c
frossstik <lehapoh@gmail.com>
2024-04-17 19:19:21 +0300

    Commit 2

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("1 коммит");
+        System.out.println("2 коммит");
     }
 
 }
